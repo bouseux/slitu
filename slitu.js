810b96b159aaf16c470a7ee8a1319aa6f7a6d2ec
Ossama <ossedb@gmail.com>
2014-07-19 19:53:42 +0200

    Added description in header

--- slitu.js
+++ slitu.js
@@ -10,6 +10,8 @@
 
 	slitu.VERSION = 'version';
 	slitu.REPO = 'https://github.com/oss6/slitu';
+	slitu.DESCRIPTION = 'Slitu is a javascript utility library that includes functions 
+						to deal with arrays, functions, numbers, objects and strings.';
 	slitu.AUTHORS = ['Edbali Ossama'];
 
 	// Private methods
